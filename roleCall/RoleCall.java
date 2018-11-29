8986b0e0314ead309cad86c6b78adb09e3a079ce
rokujohaven <43080066+rokujohaven@users.noreply.github.com>
2018-11-28 23:42:38 -0500

    Add files via upload

--- roleCall/RoleCall.java
+++ roleCall/RoleCall.java
@@ -7,154 +7,32 @@ public class RoleCall {
 	
 	public static void main(String[] args) {
 		
-		//part 1 continued: 
-//		Create three instances of type Student. 
-//		Read the values for those students from the user’s input.
-//	
-//		Print out the names of those students, their grades, 
-//		and their GPAs. Then, print out the average GPA of all three students.
-//		
-		String name; 
-		int[] grades = new int[3]; 
-		double gpa;
-		
-		Scanner s = new Scanner(System.in); 
-		
-		System.out.println("Part 1");
-		System.out.println("What is the student's name:");
-		name = s.next(); 
-		System.out.println("What are their 3 most recent grades?");
-		for (int i = 0; i < 3; ++i) {
-			grades[i] = s.nextInt();
-		}
-		System.out.println("What is their GPA?"); 
-		gpa = s.nextDouble(); 
-		
-		Student Helen = new Student(name, grades, gpa); 
-		
-		Helen.Profile(); 
-		/////
-		
-		System.out.println("What is the student's name:");
-		name = s.next(); 
-		System.out.println("What are their 3 most recent grades?");
-		for (int i = 0; i < 3; ++i) {
-			grades[i] = s.nextInt();
-		}
-		System.out.println("What is their GPA?"); 
-		gpa = s.nextDouble(); 
-		
-		Student Cindy = new Student(name, grades, gpa); 
-		Cindy.Profile();
-		
-		///
-		
-		System.out.println("What is the student's name:");
-		name = s.next(); 
-		System.out.println("What are their 3 most recent grades?");
-		for (int i = 0; i < 3; ++i) {
-			grades[i] = s.nextInt();
-		}
-		System.out.println("What is their GPA?"); 
-		gpa = s.nextDouble(); 
-		
-		Student Zack = new Student(name, grades, gpa); 
-		Zack.Profile();
-		
-		////Avg GPA of Helen, Cindy, and Zack
-		
-		System.out.println("Average GPA: " + (Helen.getGPA() + Cindy.getGPA() + Zack.getGPA())/3); 
-	
-//		2. Array of Students
-//
-//		Create an array of type Student with three positions. 
-//		Read in their info just like before.
-//
-//		Print out the names of those students, their grades, and their GPAs. 
-//		Then, print out the average GPA of all three students.
-//		
-		System.out.println();
-		System.out.println("Part 2"); 
-		Student[] people = new Student[3];
-		
-		for (int i = 0; i < 3; ++i) {
-			
-			System.out.println("What is the student's name:");
-			name = s.next(); 
-			System.out.println("What are their 3 most recent grades?");
-			for (int j = 0; j < 3; ++j) {
-				grades[j] = s.nextInt();
-			}
-			System.out.println("What is their GPA?"); 
-			gpa = s.nextDouble(); 
-			
-			people[i] = new Student(name, grades, gpa); 
-			people[i].Profile();	
-			
-		}
-		
-		//get average GPA of the 3 students in people 
-		System.out.println("Average GPA: " + (people[0].getGPA() + people[1].getGPA() + people[2].getGPA())/3); 
-		
-//		3. Modify the Length of the Array
-//
-//		Ask the user to input the number of students 
-//		they would like to enter. Store this in an integer named numStudents. 
-//		Create an array of type Student with [numStudents] positions. 
-//		Read through their info using a loop.
-//
-//		Using a second loop, print out the names of those students, 
-//		their grades, and their GPAs. Finally, print the average GPA of all students.
-		
-		System.out.println();
-		System.out.println("Part 3"); 
-		System.out.println("Please enter the number of students in the classroom:");
-		
-		int numStudents; 
-		numStudents = s.nextInt(); 
-		
-		Student[] classRoom = new Student[numStudents]; 
-		
-		//reading in Student's info
-		for (int i = 0; i < numStudents; ++i) {
-			
-			System.out.println("What is the student's name:");
-			name = s.next(); 
-			System.out.println("What are their 3 most recent grades?");
-			for (int j = 0; j < 3; ++j) {
-				grades[j] = s.nextInt();
-			}
-			System.out.println("What is their GPA?"); 
-			gpa = s.nextDouble(); 
-			
-			classRoom[i] = new Student(name, grades, gpa); 
-			
-			//printing out names of those students, grades, and GPAs. 
-			classRoom[i].Profile();
-			
-		}
-		
-		System.out.println("The average GPA of the class is: ");
-		System.out.println(AverageGPA(classRoom)); 
 
-		//part 4 continued: 
-//		in your main method, ask the user to 
-//		input the number of students they would 
-//		like to enter and store this in an integer 
-//		named numStudents. (see above)
-//	
-//		Create an array of type Student with 
-//		[numStudents] positions and 
-//		read their information through a loop. (see above) 
-//		Using another loop, have each student talk 
-//		about their school life.
+//		Allows a professor to register a classroom of students, input their grades, and calculate GPA
+		
+		System.out.println("Hello professor, how many students are in your class?");
+		Scanner s = new Scanner(System.in);
+		int numPeople = s.nextInt(); 
+		
+		//creates a class of size numPeople
+		Student[] classRoom = addClass(numPeople);
+		
+		///Print class profile for each student
+		
+		System.out.println("Class Profile: ");
+		for (int i = 0; i < numPeople; ++i) {
+			classRoom[i].Profile();
+		}
+		////Print class average
+		System.out.println("Average class GPA is: " + AverageGPA(classRoom)); 
+		System.out.println();
+		//part 4: 
 		
 		//talk about school life
+		System.out.println("Now, the students will introduce themselves...");
 		System.out.println();
-		System.out.println("Part 4");
-		System.out.println("Now, let's introduce ourselves: ");
 		
-		for (int i = 0; i < classRoom.length; ++i) {
+		for (int i = 0; i < numPeople; ++i) {
 			classRoom[i].getInfo();
 		}
 	
@@ -184,48 +62,13 @@ public class RoleCall {
 //		- Finally, using another loop, have each student talk about 
 //		their school life and their current course
 		
-		System.out.println("Here are all the courses we are offering this year: ");
-		
-		for (int i = 0; i < catalog.length; ++i) {
-			System.out.println(i + ". " + catalog[i].getCode() + " - " + catalog[i].getSubject());
-		}
-		
-		//initializing courses for entire class 
-		for (int i = 0; i < classRoom.length; ++i) {
-		System.out.println(classRoom[i].getName() + ", which course would you like to take? \n Press 0, 1 or 2 ");
-		
-		int choice = s.nextInt(); 
-		boolean done = false; 
-		while(done == false) {
-			
-			switch(choice) {
-				case 0: 
-					classRoom[i].addCourse(catalog[0]);
-					done = true; 
-					break;
-				case 1: 
-					classRoom[i].addCourse(catalog[1]);
-					done = true;
-					break;
-				case 2: 
-					classRoom[i].addCourse(catalog[2]); 
-					done = true;
-					break; 
-				default:
-					System.out.println(classRoom[i].getName() + ", you didn't enter a valid selection. \n"
-							+ "Please select either 0, 1 or 2 ONLY!");
-					choice = s.nextInt();
-					
-				}
-			}
-		
-		}
+		selectCourse(classRoom, catalog); 
 		
 		//close the scanner! 
 		s.close(); 
 		System.out.println();
 		System.out.println("Now each student will re-introduce themselves and talk about their chosen courses!");
-	
+		System.out.println();
 		for (int i = 0; i < classRoom.length; ++i) {
 			classRoom[i].getInfo();
 			classRoom[i].getCourse();
@@ -233,6 +76,54 @@ public class RoleCall {
 		
 	}
 	
+	//part 1 continued: 
+//	Create three instances of type Student. 
+//	Read the values for those students from the user’s input.
+//
+//	Print out the names of those students, their grades, 
+//	and their GPAs. Then, print out the average GPA of all three students.
+//	
+	public static Student addStudent(Student person, int numTests) {
+		
+		//this method initializes an already declared Student object and returns this object
+		Scanner s = new Scanner(System.in); 
+		String name; 
+		System.out.println("What is the student's name:");
+		name = s.next(); 
+		System.out.println("What are their " + numTests + " most recent grades?");
+		int[] g = new int[numTests]; 
+		for (int i = 0; i < g.length; ++i) {
+			g[i] = s.nextInt();
+		}
+
+		person.setName(name);
+		person.setGrades(g);
+		
+		return person; 
+		
+	}
+	public static Student[] addClass(int classSize) {
+		
+		//this method creates a new class of size classSize
+		//and returns an array of Students
+		
+		//not void because classRoom array was not created in the main, we create it here.  
+		
+		Student[] classRoom = new Student[classSize]; 
+		Scanner s = new Scanner(System.in); 
+		System.out.println("How many tests do they have?");
+		int noTests; 
+		noTests = s.nextInt();
+		
+		
+		for (int i =0; i < classSize; ++i) {
+			classRoom[i] = addStudent(new Student(), noTests);
+		}
+		
+		return classRoom;
+		
+	}
+
 	//returns the average GPA of a class of students 
 	public static double AverageGPA(Student[] classRoom) {
 		
@@ -243,5 +134,38 @@ public class RoleCall {
 		return avg/(classRoom.length); 
 	}
 	
-
+	public static void selectCourse(Student[] classRoom, Course[] catalog) {
+		
+		//this method guides each student in the class to select 1 course from the catalog
+		//void because arrays pass by reference 
+		System.out.println("Here are all the courses we are offering this year: ");
+		Scanner s = new Scanner(System.in);
+		
+		//print available courses
+		for (int i = 0; i < catalog.length; ++i) {
+			System.out.println((i+1) + ") " + catalog[i].getCode() + " - " + catalog[i].getSubject()
+					+ " with " + catalog[i].getInstructor());
+		}
+		
+		//initializing courses for entire class 
+		for (int i = 0; i < classRoom.length; ++i) {
+		System.out.println(classRoom[i].getName() + ", which course would you like to take? ");
+		
+		int choice = s.nextInt(); 
+		boolean done = false; 
+		while (!done) {
+		if (choice > 0 && choice <= catalog.length) {
+			classRoom[i].addCourse(catalog[choice-1]);
+			done = true; 
+			}
+		else {
+			System.out.println(classRoom[i].getName() + ", you didn't enter a valid selection. \n"
+					+ "Please try again.");
+			choice = s.nextInt();
+				}
+				
+			}
+		
+		}
+	}
 }
